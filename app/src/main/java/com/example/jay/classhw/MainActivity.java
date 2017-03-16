7a9eaca41bea395445c9fb5bc6b578d8efaad65c
miyuiki-notebook <a121406@gmail.com>
2017-03-17 03:00:11 +0800

    committed from ubuntu-note

--- app/src/main/java/com/example/jay/classhw/MainActivity.java
+++ app/src/main/java/com/example/jay/classhw/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.jay.classhw;
 
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private TextView input,output;
@@ -61,9 +59,7 @@ public class MainActivity extends AppCompatActivity {
             });
             obj.setNegativeButton("不要！", new DialogInterface.OnClickListener() {
                 @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
+                public void onClick(DialogInterface dialog, int which) {}
             });
             obj.show();
         }
